774b7d8a06d3359202ed5df7aa1e9f8930ad8cec
Lior Hirschfeld <Lior Hirschfeld>
2016-12-26 18:24:29 -0500

    Completed first version of the bot

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# GoodreadsBot
+# PathOfExamineBot
 
-A reddit bot.
+Running this script requires the following prerequisites:
+ * [Python](https://www.python.org/) 3.0+
+ * [PRAW](https://praw.readthedocs.org/en/latest/)
+ * [OAuth2Util](https://github.com/SmBe19/praw-OAuth2Util)
+ * [Beautiful Soup](https://www.crummy.com/software/BeautifulSoup/) 4
+
+### Instructions
+The aim of this bot is to provide easy access to information about books and authors on [Reddit](www.redditcom). Information is gathered from [Goodreads](http://goodreads.com).
+
+This information can be accessed by typing the title of a book, an author's name, or an ISBN number in [[double square brackets]] in a comment on any of the designated subreddits. This list includes:
+ * [/r/test](http://www.reddit.com/r/test)
---
+++ litbot.py
@@ -0,0 +1,52 @@
+import re
+from bs4 import BeautifulSoup
+from time import sleep
+import pickle
+
+import praw
+from lookup import findItem
+
+r = praw.Reddit('bot2')
+
+m = re.compile(r"Info:.*")
+
+def respond(lim, rate, subs):
+    with open('ids.pickle', 'rb') as handle:
+        ids = pickle.load(handle)
+    while True:
+        for sub in subs:
+            subreddit = r.subreddit(sub)
+            for submission in subreddit.new(limit=lim):
+                comment_queue = submission.comments[:]
+                while comment_queue:
+                    com = comment_queue.pop(0)
+                    if "Info:" in com.body and com.id not in ids:
+                        print("Found Comment:" + com.id)
+                        reply = ""
+                        for item in m.findall(com.body)[:10]:
+                            for s in item[5:].split(","):
+                                try:
+                                    temp = findItem(s)
+                                except:
+                                    temp = ""
+                                reply += temp
+                                if temp != "":
+                                    reply += "\n\n---------\n\n"
+                                sleep(1)
+                        if reply != "":
+                            reply += " ^I ^am ^a ^bot."
+                            reply += " ^Reply ^to ^me ^with ^up ^to ^7 ^[[item ^names]]."
+                            reply += " ^Please ^contact ^/u/liortulip"
+                            reply += " ^with ^any ^questions ^or ^concerns."
+                            print("Replying...", end=" ")
+                            com.reply(reply)
+                            print("Replied")
+                        else:
+                            print("False Reply ^")
+                        ids.append(com.id)
+                    comment_queue.extend(com.replies)
+        with open('ids.pickle', 'wb') as handle:
+            pickle.dump(ids, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        sleep(rate)
+
+respond(50,10, ["test"])
